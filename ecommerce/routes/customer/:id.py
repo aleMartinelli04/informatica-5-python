bda4d6c4e558c963d8e0932e7e6e940c987dbf5a
alessandro <martinellialessandro04@gmail.com>
2023-04-13 17:38:54 +0200

    added types

--- ecommerce/routes/customer/:id.py
+++ ecommerce/routes/customer/:id.py
@@ -1,10 +1,12 @@
 from __main__ import app
 
+from mysql.connector.cursor import MySQLCursor
+
 from ecommerce.api import database
 
 
 @app.route('/customer/<int:customer_id>')
 @database
-def get_customer_by_id(cursor, customer_id):
+def get_customer_by_id(cursor: MySQLCursor, customer_id: int):
     cursor.execute('SELECT * FROM customer WHERE id = %s', (customer_id,))
     return cursor.fetchone()
