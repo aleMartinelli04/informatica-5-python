fc09630dda59c522702829c6e76da2262eda542f
alessandro <martinellialessandro04@gmail.com>
2023-04-20 10:22:41 +0200

    lol

--- ecommerce/routes/customer/id/orders.py
+++ ecommerce/routes/customer/id/orders.py
@@ -6,14 +6,14 @@ from ecommerce.api import database
 from utils.StatusCodeException import StatusCodeException
 
 
-@app.route('/customer/<int:customer_id>')
+@app.route('/customer/<int:customer_id>/orders')
 @database
-def get_customer_by_id(cursor: MySQLCursor, customer_id: int):
-    cursor.execute('SELECT * FROM customer WHERE id = %s', (customer_id,))
-
+def get_customer_orders(cursor: MySQLCursor, customer_id: int):
+    cursor.execute("SELECT * FROM customer WHERE id=%s", (customer_id,))
     customer = cursor.fetchone()
 
     if customer is None:
         raise StatusCodeException('Customer not found', 404)
 
-    return customer
+    cursor.execute("SELECT * FROM request WHERE customerId=%s", (customer_id,))
+    return cursor.fetchall()
